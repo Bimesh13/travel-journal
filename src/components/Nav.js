d568830ef23b2743e17cf6bb73d1fb07d1da3279
Bimesh13 <bimesh.shrestha13@gmail.com>
2022-04-04 13:29:20 +0545

    Navbar was completed.

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav>
+      <img src="images/logo.png" className="logo--image" />
+      <p className="logo--text">my travel journal.</p>
+    </nav>
+  );
+}
